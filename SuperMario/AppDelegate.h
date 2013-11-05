ac09024f7bdaafa9d4608df698d5127d88cdfa62
jashon <jashon@jashonmatoMac.local>
2013-11-05 19:18:35 +0800

    Initial Commit

---
+++ SuperMario/AppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.h
+//  SuperMario
+//
+//  Created by jashon on 13-11-5.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+@interface AppController : NSObject <UIApplicationDelegate, CCDirectorDelegate>
+{
+	UIWindow *window_;
+	UINavigationController *navController_;
+	
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) UINavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
---
+++ SuperMario/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SuperMario
+//
+//  Created by jashon on 13-11-5.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
